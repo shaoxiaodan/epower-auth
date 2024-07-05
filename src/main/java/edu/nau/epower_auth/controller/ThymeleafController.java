ddd6722686c9a2f01844c180094d338bd65d539e
shaoxiaodan <sandays_cn@163.com>
2024-07-05 12:14:28 -0700

    更新模板配置
    修复无法跳转thymeleaf对应的html的问题

--- src/main/java/edu/nau/epower_auth/controller/ThymeleafController.java
+++ src/main/java/edu/nau/epower_auth/controller/ThymeleafController.java
@@ -1,18 +1,26 @@
 package edu.nau.epower_auth.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
 
-	@GetMapping("hello")
+	@GetMapping("index")
 	public String index() {
 		
+		System.out.println("ThymeleafController :: index");
 		//无需添加后缀，在配置文件中已经指定了页面返回后缀
 		return "index";
 	}
 	
+	@GetMapping("ok")
+	public String ok() {
+		
+		System.out.println("ThymeleafController :: ok");
+		//无需添加后缀，在配置文件中已经指定了页面返回后缀
+		return "ok";
+	}
 }
