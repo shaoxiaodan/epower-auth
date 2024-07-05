f31f00ce89c53f7d43976573366027be66b9b4e1
shaoxiaodan <sandays_cn@163.com>
2024-07-05 10:22:42 -0700

    init project
    init and create a new project for nau-693

---
+++ src/test/java/edu/nau/epower_auth/EpowerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nau.epower_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpowerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
