3dd851ae76b916853058f81593e18abc4f245f70
shaoxiaodan <sandays_cn@163.com>
2024-07-07 01:28:05 -0700

    创建授权映射mapper

---
+++ src/main/java/edu/nau/epower_auth/mapper/AuthMapper.java
@@ -0,0 +1,19 @@
+package edu.nau.epower_auth.mapper;
+
+/**
+ * 授权映射
+ * 
+ * @ClassName: AuthMapper
+ * @Description: TODO
+ * @author Xiaodan Shao(xs94@nau.edu)
+ * @date 2024-07-07 01:23:29
+ */
+public class AuthMapper {
+
+	// 用户-角色授权
+
+	// 角色-菜单授权
+
+	// 菜单-资源路径授权
+
+}
