e4f3e173bd970b9c99cde44f633dddee455cf8d6
shaoxiaodan <sandays_cn@163.com>
2024-07-06 21:25:56 -0700

    添加Menu映射类mapper
    添加Menu映射类mapper

--- src/main/java/edu/nau/epower_auth/mapper/MenuMapper.java
+++ src/main/java/edu/nau/epower_auth/mapper/MenuMapper.java
@@ -1,5 +1,21 @@
 package edu.nau.epower_auth.mapper;
 
-public class MenuMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import edu.nau.epower_auth.dao.Menu;
+
+/**
+ * 
+ * @ClassName: MenuMapper
+ * @Description: TODO
+ * @author Xiaodan Shao(xs94@nau.edu)
+ * @date 2024-07-06 09:24:18
+ */
+public interface MenuMapper {
+
+	@Select("")
+	public List<Menu> findMenuByRoleId(@Param("roleId") int roleId);
 }
