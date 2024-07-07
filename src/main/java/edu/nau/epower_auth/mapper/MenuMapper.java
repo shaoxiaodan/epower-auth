04c351000d4a73940c42b004c2679809e8690e7b
shaoxiaodan <sandays_cn@163.com>
2024-07-06 21:27:23 -0700

    添加Menu映射执行sql
    添加Menu映射执行sql

--- src/main/java/edu/nau/epower_auth/mapper/MenuMapper.java
+++ src/main/java/edu/nau/epower_auth/mapper/MenuMapper.java
@@ -16,6 +16,9 @@ import edu.nau.epower_auth.dao.Menu;
  */
 public interface MenuMapper {
 
-	@Select("")
+	@Select("select m.id as id, m.name as name, m.description as description"
+			+ "	from role_menu rm"
+			+ "	left join menu m on rm.menu_id = m.id"
+			+ "	where rm.role_id = #{roleId}")
 	public List<Menu> findMenuByRoleId(@Param("roleId") int roleId);
 }
