c8969ac7273fc87b29961cb0843bd31858b6fc69
shaoxiaodan <sandays_cn@163.com>
2024-07-06 22:19:02 -0700

    Update EpowerAuthApplication.java

--- src/main/java/edu/nau/epower_auth/EpowerAuthApplication.java
+++ src/main/java/edu/nau/epower_auth/EpowerAuthApplication.java
@@ -3,8 +3,15 @@ package edu.nau.epower_auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * 程序入口
+ * 
+ * @ClassName: EpowerAuthApplication
+ * @Description: TODO
+ * @author Xiaodan Shao(xs94@nau.edu)
+ * @date 2024-07-06 10:17:55
+ */
 @SpringBootApplication
 @MapperScan("edu.nau.epower_auth.mapper")
 public class EpowerAuthApplication {
