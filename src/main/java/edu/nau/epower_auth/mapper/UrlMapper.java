3ad5195d2bf402dd4701161b46077a3a246a6b3d
shaoxiaodan <sandays_cn@163.com>
2024-07-06 22:33:03 -0700

    更新url映射sql
    更新url映射sql

--- src/main/java/edu/nau/epower_auth/mapper/UrlMapper.java
+++ src/main/java/edu/nau/epower_auth/mapper/UrlMapper.java
@@ -17,7 +17,9 @@ import edu.nau.epower_auth.dao.Url;
  */
 public interface UrlMapper {
 
-	@Select("select ul.id as id, ul.path as path" + " from menu_url mu" + " left join url u on mu.url_id = u.id"
+	@Select("select ul.id as id, ul.path as path" 
+			+ " from menu_url mu" 
+			+ " left join url ul on mu.url_id = ul.id"
 			+ " where mu.menu_id = #{menuId}")
 	public List<Url> findUrlByMenuId(@Param("menuId") int menuId);
 
--- src/test/java/edu/nau/epower_auth/UrlMapperTest.java
+++ src/test/java/edu/nau/epower_auth/UrlMapperTest.java
@@ -11,6 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import edu.nau.epower_auth.dao.Url;
 import edu.nau.epower_auth.mapper.UrlMapper;
 
+/**
+ * 
+ * @ClassName: UrlMapperTest
+ * @Description: TODO
+ * @author Xiaodan Shao(xs94@nau.edu)
+ * @date 2024-07-06 10:28:34
+ */
 @SpringBootTest
 public class UrlMapperTest {
 
@@ -18,12 +25,24 @@ public class UrlMapperTest {
 	private UrlMapper urlMapper;
 
 	@Test
-	public void f() {
+	public void testFindUrlByMenuId() {
 
-		List<Url> urlList = urlMapper.findUrlByMenuId(1);
+		int menuId = 0;
+		menuId = 1; //用户管理菜单
+//		menuId = 2; //角色管理菜单
+//		menuId = 3; //菜单管理菜单
+//		menuId = 4; //URL管理菜单
+//		menuId = 5; //用户授权菜单
+//		menuId = 6; // 角色授权菜单
+//		menuId = 7; // null
+
+		List<Url> urlList = urlMapper.findUrlByMenuId(menuId);
 		if (urlList != null && urlList.size() > 0) {
+			Url url = null;
 			for (int i = 0; i < urlList.size(); i++) {
-				System.out.println("testFindUrlByMenuId::urlList=" + urlList.get(i));
+				url = urlList.get(i);
+				System.out.println("testFindUrlByMenuId::urlList=" + url.getId() + "\t" + url.getPath() + "\t"
+						+ url.getDescription());
 			}
 		}
 
