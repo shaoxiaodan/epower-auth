b2d9f860dfb8ff575385b1f129572b9ae29c69a3
shaoxiaodan <sandays_cn@163.com>
2024-07-07 23:42:06 -0700

    添加菜单-资源路径接口

---
+++ src/main/java/edu/nau/epower_auth/service/MenuService.java
@@ -0,0 +1,35 @@
+package edu.nau.epower_auth.service;
+
+import java.util.List;
+
+import edu.nau.epower_auth.dao.Menu;
+import edu.nau.epower_auth.dao.MenuUrl;
+
+/**
+ * 菜单服务接口
+ * 
+ * @ClassName: MenuService
+ * @Description: TODO
+ * @author Xiaodan Shao(xs94@nau.edu)
+ * @date 2024-07-07 11:36:40
+ */
+public interface MenuService {
+
+	// 菜单列表
+	public List<Menu> listMenu();
+
+	// 添加菜单
+	public int addMenu(Menu menu);
+
+	// 更新菜单
+	public int updateMenu(Menu menu);
+
+	// 删除菜单
+	public int removeMenu(Menu menu);
+
+	// 添加菜单资源路径授权
+	public int addMenuUrlAuth(MenuUrl menuUrl);
+
+	// 删除菜单资源路径授权
+	public int removeMenuUrlAuth(MenuUrl menuUrl);
+}
